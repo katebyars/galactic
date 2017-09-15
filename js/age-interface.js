3e662f52b6bd65bde1a7dcbf613cb920301339e4
Guest User <Guest@Epicodus-6D.local>
2017-09-15 12:30:40 -0700

    add index.html form items.

--- js/age-interface.js
+++ js/age-interface.js
@@ -1,5 +1,4 @@
 import { Age } from './../js/age.js';
-import { Person } from './../js/person.js';
 import { Birthday } from './../js/birthday.js';
 
 $(document).ready(function() {
@@ -10,6 +9,10 @@ $(document).ready(function() {
     let year = $("#year").val();
     let birthday = month.concat(" ", day, ", ", year, " 12:00:01");
 
+    let smoker = $("#smoker").val();
+    let diet = $("#diet").val();
+    let stress = $("#stress").val();
+
     let yourBirthday = new Birthday(birthday);
     let yourAge = yourBirthday.calculateAge();
     let earthAge = new Age(yourAge);
@@ -17,10 +20,14 @@ $(document).ready(function() {
     let jupiter = earthAge.ageOnJupiter(yourAge);
     let mars = earthAge.ageOnMars(yourAge);
     let mercury = earthAge.ageOnMercury(yourAge);
-    let newPerson = new Person(earthAge, venus, jupiter, mars, mercury);
+
+    let lifeExpectancyOnEarth = earthAge.lifeExpectancyOnEarth(yourAge, smoker, stress, diet);
+
     $("#output").append("<li> Your age on Earth:  " + yourAge + "</li>" +"<li> Your age on Venus:  " + venus + "</li>" +"<li> Your age on Jupiter:  " + jupiter + "</li>" + "<li> Your age on Mars:  " + mars + "</li>" + "<li> Your age on Mercury:  " + mercury + "</li>");
 
-    $("input").val("");
+    $("vitalSigns").append("<li> On earth: " +  lifeExpectancyOnEarth);
 
+    $("input").val("");
   });
+
 });
